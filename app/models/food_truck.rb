750cbf06b6cf5d6565f73e54b3b5a95c232e8ed7
Kevin Collins <kevincol54@gmail.com>
2014-06-19 17:16:41 -0400

    more stuff

--- app/models/food_truck.rb
+++ app/models/food_truck.rb
@@ -6,13 +6,25 @@ class FoodTruck < ActiveRecord::Base
   after_validation :geocode 
   has_attached_file :image,
     :storage => :dropbox,
-    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+    :dropbox_credentials => { app_key: ENV['APP_KEY'],
+                              app_secret: ENV['APP_SECRET'],
+                              access_token: ENV['ACCESS_TOKEN'],
+                              access_token_secret: ENV['ACCESS_TOKEN_SECRET'],
+                              user_id: ENV['USER_ID'],
+                              access_type: 'app_folder'}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   validates :name, :description, :address, presence: true 
   after_save :find_user_and_send_message
   
   enum status: [:closed, :serving]
- 
+  p 'hey kevin'
+  p  ENV['APP_KEY']
+  p ENV['APP_SECRET']
+  p ENV['ACCESS_TOKEN']
+  p ENV['ACCESS_TOKEN_SECRET']
+  p ENV['USER_ID']
+
+
 
   def current_user_likes_this_truck(current_user)
     current_user.likes.where(food_truck_id: self.id, status: 1)
